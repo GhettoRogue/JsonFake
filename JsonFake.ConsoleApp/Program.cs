539591f2c61c0efb9282beb2d445713aa7cab251
Starinin Andrey <starininandrey@gmail.com>
2023-10-06 20:09:09 +0300

    Написание программы

--- JsonFake.ConsoleApp/Program.cs
+++ JsonFake.ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JsonFake.Lib;
+
+Console.Write("Введите id пользователя: ");
+var id = Convert.ToInt32(Console.ReadLine());
+var userApi = new UserAPI();
+var user = userApi.GetUserById(id);
+
+if (user is null)
+{
+    Console.WriteLine("Такого пользователя нет!");
+    return;
+}
+
+Console.WriteLine($"{user.Id}: {user.Name}, {user.Email}");
+
