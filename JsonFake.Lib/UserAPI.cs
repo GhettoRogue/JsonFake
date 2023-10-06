85ece634f4f85c2b3dee8bfc212e87ea855b5cf6
Starinin Andrey <starininandrey@gmail.com>
2023-10-06 19:49:55 +0300

    Добавление класса для запроса данных из API

---
+++ JsonFake.Lib/UserAPI.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using JsonFake.Lib.Models;
+
+namespace JsonFake.Lib;
+
+public class UserAPI
+{
+    private readonly HttpClient _http;
+
+    public UserAPI()
+    {
+        _http = new HttpClient()
+        {
+            BaseAddress = new Uri("https://jsonplaceholder.typicode.com")
+        };
+    }
+    
+    public User? GetUserById(int id)
+    {
+        var response = _http.GetAsync($"users?id={id}").Result;
+        var json = response.Content.ReadAsStringAsync().Result;
+        return JsonSerializer.Deserialize<IEnumerable<User>>(json)?.FirstOrDefault();
+    }
+}
